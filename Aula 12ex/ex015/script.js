e2b719ada6be02bd6deab2a929c07c5c1f70aa80
chiconunes <fcn_jr@hotmail.com>
2022-03-29 21:05:43 -0300

    Aula 21 - Exercicios Parte 3

---
+++ Aula 12ex/ex015/script.js
@@ -0,0 +1,57 @@
+function verificar() {
+    var data = new Date()
+    var ano  = data.getFullYear()
+    var fano = document.getElementById("txtano")
+    var res  = document.querySelector('div#res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano){     
+        window.alert(`[Erro] verifique os dados e tente novamente`)
+    }
+    else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+
+        if (fsex[0].checked){
+            gênero = 'Homem' 
+            if (idade>=0  && idade < 10) {
+                //crianca       
+                img.setAttribute('src','bebe_homem.jpg')  
+            }else if (idade < 21) {
+                //jovem            
+                img.setAttribute('src','jovem_homem.jpg')         
+            }
+            else if (idade < 50) {
+                //adulto       
+                img.setAttribute('src','adulto_homem.jpg')  
+            }else{
+                //idoso   
+                img.setAttribute('src','idoso_homem.jpg')              
+            }
+
+        }else{
+            gênero = 'Mulher'
+            if (idade >=  0 && idade <= 10) {
+                //crianca       
+                img.setAttribute('src','bebe_mulher.jpg')         
+            }else if (idade < 21) {
+                //jovem            
+                img.setAttribute('src','jovem_mulher.jpg')         
+            }
+            else if (idade < 50) {
+                //adulto       
+                img.setAttribute('src','adulta_mulher.jpg')  
+            }else{
+                //idoso   
+                img.setAttribute('src','idosa_mulher.jpg')              
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.appendChild(img)
+    }
+ 
+    
+}
